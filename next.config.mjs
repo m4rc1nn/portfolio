bef586ab353f2bb9196b2d9f5ba11319f7e7a591
root <mkowalczyk193@gmail.com>
2024-08-31 19:02:08 +0200

    zmiana optymalizacji zdjec

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
-import nextMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
-import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
-  images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com"],
-  },
-  experimental: {
-    mdxRs: true,
-  },
+    images: {
+        unoptimized: true,
+    },
 };
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
